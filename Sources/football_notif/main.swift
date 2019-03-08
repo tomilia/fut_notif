87eb9686c1130344ee44d2dab5c34af70106c016
tomilia <koekax@gmail.com>
2019-03-09 02:12:09 +0800

    tomilia: corner implementation

--- Sources/football_notif/main.swift
+++ Sources/football_notif/main.swift
@@ -7,6 +7,17 @@ var diff_string=""
 var timer: Timer!
 var base_url="http://www.goaloo.com/CalcTime.aspx?"
 var isFinished=false
+
+/*
+ Implementing: corner api
+ http://data.goaloo.com/history/corner.aspx?id={id}&companyid=8
+ */
+func get_corner(id:String)
+{
+    
+  //  print(try "corner:"+(doc.select("td tr")).get(2).text())
+}
+
 func get_match_status(match_id:String)
 {
     let myURLString = base_url+match_id
@@ -26,9 +37,9 @@ func get_match_status(match_id:String)
                 isFinished=true;
                 let task = Process()
                 task.launchPath = "/usr/bin/osascript"
-                print(diff_string)
                 
-                let arg = "-e "+"display notification \""+diff_string+"\" sound name \"default\" with title \"FULL TIME!!\" "
+                
+                let arg = "-e "+"display notification \""+diff_string+"\" sound name \"Glass\" with title \"Full time\" "
                 
                 
                 task.arguments = [arg]
@@ -63,8 +74,10 @@ func crawler(url:String){
             
             let doc: Document = try SwiftSoup.parse(result_string)
             let temp_str = try doc.select("div#home .name").text()+" "+doc.select("span.b.t15").text()+" "+doc.select("div#guest .name").text()
+            
             //jsData/(0,2)/(2,4)/id.js?v=1
             let match_status = try base_url+doc.select("strong script").attr("src")
+            
             let malform = match_status.components(separatedBy: "&")
             
             //doc diff than string : match score change
@@ -87,7 +100,7 @@ func crawler(url:String){
             }
             if(!isFinished)
             {
-                get_match_status(match_id: malform[2])
+               get_match_status(match_id: malform[2])
             }
         } catch Exception.Error(let type, let message) {
             print(message)
@@ -105,6 +118,7 @@ print("Enter futbol link:")
 let response = readLine()
 while(true)
 {
+    
     DispatchQueue.global(qos: .background).async {
         crawler(url:response!)
     }
